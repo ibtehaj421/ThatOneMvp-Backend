1e7e0928a7cfc32a9df13887183259a12fe2ed7e
ibtehaj421 <ibtehajkazmi09@gmail.com>
2026-04-01 11:52:39 +0500

    files: added docker files

--- database/db.go
+++ database/db.go
@@ -2,8 +2,8 @@ package database
 
 import (
 	"log"
-	
-
+	"os"
+	"fmt"
 	"health/anam/backend/models" // Update with your actual module path
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -14,8 +14,15 @@ var DB *gorm.DB
 
 func ConnectDB() {
 	
-	dsn := "host=127.0.0.1 user=postgres password=mysecretpassword dbname=postgres port=5435 sslmode=disable TimeZone=Asia/Karachi"
+	//dsn := "host=127.0.0.1 user=postgres password=mysecretpassword dbname=postgres port=5435 sslmode=disable TimeZone=Asia/Karachi"
+	host := getEnv("DB_HOST", "localhost")
+	user := getEnv("DB_USER", "postgres")
+	password := getEnv("DB_PASSWORD", "postgres")
+	dbName := getEnv("DB_NAME", "postgres")
+	port := getEnv("DB_PORT", "5432")
 
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Karachi", 
+		host, user, password, dbName, port)
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info), // Prints SQL queries to console
@@ -34,4 +41,11 @@ func ConnectDB() {
 	}
 
 	log.Println("Database schemas migrated successfully")
+}
+
+func getEnv(key, fallback string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return fallback
 }
